7ee62b75eadfd68d645a2033b5dd2b9118d40ebb
MatanyaP <matanperetz314@gmail.com>
2025-01-15 15:56:03 +0200

    Add password

--- main.py
+++ main.py
@@ -477,7 +477,37 @@ class OpenAIRealtimeAPIWrapper:
         self._play_stream = av.audio.fifo.AudioFifo()
 
 
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password incorrect, show input + error
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct
+        return True
+
 def main():
+    if not check_password():
+            st.stop()
     loop = get_event_loop(_logger = logger)
 
     # Load article contexts from the articles directory
